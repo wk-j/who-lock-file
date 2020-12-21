3c8e648ffbdd68e462a6ab5fb1a21a612d5423fa
wk <somnuk.wk@gmail.com>
2020-12-21 13:15:42 +0700

    Added actions script

---
+++ README.md
@@ -0,0 +1,17 @@
+## Who Lock
+
+[![Actions](https://github.com/wk-j/who-lock-file/workflows/NuGet/badge.svg)](https://github.com/wk-j/who-lock-file/actions)
+[![NuGet](https://img.shields.io/nuget/v/wk.WhoLock.svg)](https://www.nuget.org/packages/wk.WhoLock)
+[![NuGet Downloads](https://img.shields.io/nuget/dt/wk.WhoLock.svg)](https://www.nuget.org/packages/wk.WhoLock)
+
+## Usage
+
+```bash
+wk-who-lock resource/vim.pdf
+```
+
+## Development
+
+```bash
+dotnet pack src/WhoLock /p:Version=0.0.1 -o __publish__
+```
--- src/WhoLock/Program.cs
+++ src/WhoLock/Program.cs
@@ -8,19 +8,27 @@ namespace WhoLock {
             if (args.Length == 0) {
                 Console.WriteLine("Invalid arguments");
                 Console.WriteLine("Usage: wk-who-lock <FilePath>");
+                return;
             }
 
             var file = args[0];
             if (!File.Exists(file)) {
                 Console.WriteLine("Invalid arguments");
                 Console.WriteLine("File not exist {0}", file);
+                return;
             }
 
             var process = FileUtil.WhoIsLocking(file);
-            var index = 1;
 
+            if (process.Count == 0) {
+                Console.WriteLine("No process is locking {0}", file);
+                return;
+            }
+
+            var index = 1;
+            Console.WriteLine("{0,3} {1, 20} {2, 80}", "#", "[Process]", "[Path]");
             foreach (var item in process) {
-                Console.WriteLine("{0,5} {1, 100}", index++, item.ProcessName);
+                Console.WriteLine("{0,3} {1, 20} {2, 80}", index++, item.ProcessName, item.MainModule.FileName);
             }
         }
     }
